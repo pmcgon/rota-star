d91557e844e3f2063cdd98e1df200511d279684d
Patrick Mc Gonagle <patrickmcg2@gmail.com>
2025-06-16 00:09:17 +0100

    Initial commit. Added React frontend and django backend with basic functions

---
+++ backend/scheduler/rota_solver.py
@@ -0,0 +1,7 @@
+def solve_rota(employees_data):
+    return {
+        "status": "ok",
+        "message": "Simulated rota for development",
+        "table": [["George", "9-5", "Off", "Eve", "Off", "9-5", "Eve", "Off"]],
+        "headers": ["Employee", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    }
