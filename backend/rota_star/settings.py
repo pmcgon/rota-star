1ca5dea8b4c84a54a43c70c2d8d8c7f72a39fd30
Patrick Mc Gonagle <patrickmcg2@gmail.com>
2025-06-16 14:50:11 +0100

    Update CORS_ALLOWED_ORIGINS to include production frontend URL

--- backend/rota_star/settings.py
+++ backend/rota_star/settings.py
@@ -131,5 +131,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # React frontend
-    # os.getenv('FRONTEND_URL', ''),  #Production frontend url
+    "https://rota-star.vercel.app/",  #Production frontend url
 ]
